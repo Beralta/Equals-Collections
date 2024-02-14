f364be53d3b72f7aa6d645a741ca066197840b6d
beralta <pedrohugoferreira@gmail.com>
2024-02-14 20:36:08 -0300

    adicionando construção do hashCode

--- src/dev/collections/com/Collections/dominio/SmartPhone.java
+++ src/dev/collections/com/Collections/dominio/SmartPhone.java
@@ -23,8 +23,18 @@ public class SmartPhone {
     //Simétrico: para X e Y diferente de true, se x.equals(y) == true logo, y.equals(x) == true
     //Transitividade: para x,y,z, diferentes de null, se x.equals(y) == true, e x.equals(z) == true, y.equals(z) == true
     //Consistente: x.equals(x) sempre retorna true se x for != null
-
     // ( Vereficar se o objeto é null, casjo seja vai retornar falso )
+
+    public int hashCode() {
+        return code == null ? 0 : this.code.hashCode();
+    }
+
+    //hashCode: É utilizado para buscar um código único de hash do objeto onde determina o local aonde o mesmo está armazenado
+
+    // Se x.equals(y) == y.equals(x), logo x.hashCode() == y.hashCode() (Sem colisão)
+    // Se y.hashCode() == x.hashCode() não cenessariamente y.equals(x) == true (Com colisão)
+
+
     public String getCode() {
         return code;
     }
